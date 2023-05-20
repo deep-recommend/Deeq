28ea9b7789894f166da79890c0482e1484f90ebb
root <root@DESKTOP-MGOMRR6.localdomain>
2023-05-21 01:46:12 +0900

    Deeq

--- deeq/main.py
+++ deeq/main.py
@@ -1,7 +1,23 @@
-class QML:
-    def __init__():
-        pass
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from torch.autograd import Function
+from torchvision import datasets, transforms
+import torch.optim as optim
+import torch.nn as nn
+import torch.nn.functional as F
+import qiskit
+from qiskit.visualizatioin import *
 
-class QNN:
-    def __init__():
+class QuantumCircuit:
+    """
+    The class implements a simple Quantum Block
+    """
+    
+    def __init__(self, num_qubits, backend, copies: int = 1000):
+        self._circuit = qiskit.QuantumCircuit(num_qubits)
+        self.theta = qiskit.circuit.Parameter('theta')
+        self._circuit_.h([i for i in range(num_qubits)])
+        
+        
         pass
